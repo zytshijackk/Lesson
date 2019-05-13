5e857bb555883a2df051f19fe4d1c70a564c8ea4
ChenHao <864171652@qq.com>
2019-05-13 17:33:46 +0800

    22

--- src/main/java/com/ch/lesson/dao/CourseMapper.java
+++ src/main/java/com/ch/lesson/dao/CourseMapper.java
@@ -1,7 +1,7 @@
-package com.ch.lesson.mapper;
+package com.ch.lesson.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ch.lesson.entity.Course;
+import com.ch.lesson.domain.Course;
 
 /**
  * <p>
--- src/main/java/com/ch/lesson/dao/DepartmentMapper.java
+++ src/main/java/com/ch/lesson/dao/DepartmentMapper.java
@@ -1,7 +1,7 @@
-package com.ch.lesson.mapper;
+package com.ch.lesson.dao;
 
-import com.ch.lesson.entity.Department;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ch.lesson.domain.Department;
 
 /**
  * <p>
